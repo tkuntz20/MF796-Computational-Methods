25221a3be6771c695bac93166b16a07ea0ba2566
tkuntz20 <thomaskuntz20@gmail.com>
2022-02-20 21:34:37 -0500

    init push

---
+++ MF796-HW-2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 20 21:05:52 2022
+
+@author: Thomas Kuntz
+"""
+
+import math
+import numpy as np
+import cmath
+import matplotlib.pyplot as plt
+import time
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.stats import norm
+#from scipy.optimize import brentq as root
+from scipy.optimize import root
+from scipy import interpolate
