3e7631eaae82a1a1a303da61a9acbc00b0419af8
tkuntz20 <thomaskuntz20@gmail.com>
2022-03-14 23:32:29 -0400

    problem one done

--- MF796-HW-4.py
+++ MF796-HW-4.py
@@ -6,6 +6,7 @@ Created on Sun Mar. 14 21:05:52 2022
 import math
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import cmath
 import scipy.stats as si
 import matplotlib.pyplot as plt
@@ -14,4 +15,57 @@ from scipy.optimize import root, minimize
 from scipy import interpolate
 import warnings
 warnings.filterwarnings("ignore")
+
+
+def importData(df):
+    print(df.head())
+    print(df.tail())
+    #print('count of null values',df.isnull().sum())
+    #print(df.describe())
+    df = df.dropna()
+    print(df.describe())
+    return df
+
+
+
+
+if __name__ == '__main__':
+
+    # problem 1, part a):
+    df = pd.read_csv('mf796-hw4-data.csv',index_col='date',infer_datetime_format=True)
+    data = importData(df)
+    data.isnull().sum()
+
+    # part b)
+    dfPct = data.pct_change()
+    dfPct = dfPct.dropna()
+    print(dfPct.head(10))
+
+    # part c)
+    plt.figure(figsize=(17, 11))
+    sns.heatmap(dfPct.cov(), cmap="Blues", annot=False)
+    plt.show()
+    cov_matrix = dfPct.cov()
+    eigenValues, eigenVectors = np.linalg.eig(cov_matrix)
+
+    plt.plot(eigenValues)
+    plt.show()
+
+    plus = sum(count > 0 for count in eigenValues)
+    minus = sum(count < 0 for count in eigenValues)
+    print(f'the number of positive eigen values is: {plus}, and the number of negative is: {minus}')
+
+    # part d)
+    evSum = eigenValues.sum()
+    evCumSum = eigenValues.cumsum()
+    temp = evCumSum / evSum
+    cum1 = len(temp[temp < 0.5]) + 1
+    cum2 = len(temp[temp < 0.9]) + 1
+    print(f'the 50% {cum1}, and the 90% {cum2}')
+
+    # part e)
+    rets = eigenVectors[:, :cum2]
+    resids = dfPct - np.dot(dfPct.values.dot(rets), rets.T)
+    plt.plot(resids)
+    plt.show()
 
