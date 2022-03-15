d3a31c97137cb268e7f13411dc43f03c2f661304
tkuntz20 <thomaskuntz20@gmail.com>
2022-03-14 22:13:35 -0400

    update

---
+++ MF796-HW-4.py
@@ -0,0 +1,17 @@
+"""
+Created on Sun Mar. 14 21:05:52 2022
+@author: Thomas Kuntz MF-796-HW-4
+"""
+
+import math
+import numpy as np
+import pandas as pd
+import cmath
+import scipy.stats as si
+import matplotlib.pyplot as plt
+import time
+from scipy.optimize import root, minimize
+from scipy import interpolate
+import warnings
+warnings.filterwarnings("ignore")
+
